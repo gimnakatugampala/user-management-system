6a90f134b887a2e9fcb09c8bb1be8a768d316350
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-06-16 14:16:21 +0530

    templates

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const redis = require('redis')
 const bodyParser = require('body-parser')
-const  exphbs  = require('express-handlebars');
+const  exphbs  = require('express-handlebars')
+const bcrypt = require('bcrypt')
 
 const app = express()
+
+const users = []
 
 // Handlebars
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
@@ -13,6 +16,33 @@ app.set('view engine', 'handlebars');
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Homepage
+app.get('/',(req,res) =>{
+    res.render('index',{name:'Gimna'})
+})
+
+// Login
+app.get('/login',(req,res) =>{
+    res.render('login',{name:'Gimna'})
+})
+
+// Login
+app.post('/login',(req,res) =>{
+    
+})
+
+
+// Register
+app.get('/register',(req,res) =>{
+    res.render('register',{name:'Gimna'})
+})
+
+// Register
+app.post('/register',(req,res) =>{
+    
+})
+
 
 
 const port = process.env.PORT || 3000
